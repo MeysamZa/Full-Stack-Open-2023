614cbe2e611701320e7776882d4ae91714f0fad6
Meysam Zahedi <Maysam.t.zahedi@gmail.com>
2024-01-19 01:14:12 +0330

    3.12: Command-line database

---
+++ Part3/Phonebook_Backend/mongo.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length<3) {
+  console.log('Password is required.')
+  process.exit(1)
+}
+else if(process.argv.length===4){
+    console.log('Number field of person is required.');
+    process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url =`mongodb+srv://MeysamZa:${password}@cluster0.uzxlipp.mongodb.net/PhonebookDB?retryWrites=true&w=majority`
+
+mongoose.set('strictQuery',false)
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if(process.argv.length===3){
+    Person.find({}).then(result=>{
+        console.log('PhoneBook:')
+        result.forEach(person=>console.log(`${person.name} ${person.number}`))
+        mongoose.connection.close()
+    })
+}
+else{
+    const person=new Person({
+        name:process.argv[3],
+        number:process.argv[4]
+    })
+    person.save().then(result=>{
+        console.log(`added ${person.name} number ${person.number} to phonebook`);
+        mongoose.connection.close()
+    })
+}
+
