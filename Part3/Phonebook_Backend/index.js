5bea071cc2a43b2ca908eb11c48586175f7ae2a1
Meysam Zahedi <Maysam.t.zahedi@gmail.com>
2024-01-18 14:25:40 +0330

    3.9 phonebook backend step9

--- Part3/Phonebook_Backend/index.js
+++ Part3/Phonebook_Backend/index.js
@@ -1,7 +1,9 @@
 const express=require('express')
 const morgan=require('morgan')
+const cors=require('cors')
 
 const app=express()
+app.use(cors())
 app.use(express.json())
 
 morgan.token('body', (req, res)=>{ return req.method==='POST' ? JSON.stringify(req.body) : null})
@@ -83,7 +85,7 @@ app.post('/api/persons',(request,responce)=>{
 })
 
 
-const PORT=3001
-app.listen(PORT,()=>{
-    console.log(`Server is running on port ${PORT}`);
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
 })
