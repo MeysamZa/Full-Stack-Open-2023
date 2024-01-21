1becad1dbe7b01088e1d5d2205b96b5727e8cb0b
Meysam Zahedi <Maysam.t.zahedi@gmail.com>
2024-01-21 16:22:31 +0330

    4.2 Blog list, step2

---
+++ Part4/BlogList/app.js
@@ -0,0 +1,25 @@
+const config=require('./utils/config')
+const logger=require('./utils/logger')
+const express = require('express')
+const cors = require('cors')
+const blogsRouter=require('./controllers/blogs')
+const middleware=require('./utils/middleware')
+const mongoose = require('mongoose')
+
+const mongoUrl = config.MONGODB_URI
+logger.info('connecting to', mongoUrl.replace(/(.+:\/\/)(\w+:)(.+)(@.+)/,'$1$2********$4'))
+mongoose.connect(mongoUrl)
+	.then(() => logger.info('Successfully connected to MongoDB'))
+	.catch(error => logger.error('Error in connecting to MongoDB:',error.message))
+
+const app = express()
+app.use(cors())
+app.use(express.static('dist'))
+app.use(express.json())
+app.use(middleware.requestLogger)
+app.use('/api/blogs',blogsRouter)
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+
+module.exports =app
