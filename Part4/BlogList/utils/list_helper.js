18b671c182570b221ef679daf2d1bb2cf077786e
Meysam Zahedi <Maysam.t.zahedi@gmail.com>
2024-01-22 11:27:39 +0330

    4.5*: helper functions and unit tests, step3

--- Part4/BlogList/utils/list_helper.js
+++ Part4/BlogList/utils/list_helper.js
@@ -9,7 +9,29 @@ const totalLikes=(blogs) => {
 	},0)
 }
 
+const favoriteBlog=(blogs) => {
+	const blogWithMostLikes = blogs.reduce((blog_With_Most_Likes,item) => {
+		if(item.likes>blog_With_Most_Likes.likes){
+			return item
+		}
+		else{
+			return blog_With_Most_Likes
+		}	}
+	,blogs[0])
+
+	if(!blogWithMostLikes){
+		return null
+	}
+
+	return {
+		title:blogWithMostLikes.title,
+		author:blogWithMostLikes.author,
+		likes:blogWithMostLikes.likes
+	}
+}
+
 module.exports={
 	dummy,
-	totalLikes
+	totalLikes,
+	favoriteBlog
 }
