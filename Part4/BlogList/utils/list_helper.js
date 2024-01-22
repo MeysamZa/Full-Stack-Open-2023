983f90d4d1f6ec12c2059d112c56b20700b8f12f
Meysam Zahedi <Maysam.t.zahedi@gmail.com>
2024-01-22 10:22:53 +0330

    4.4: helper functions and unit tests, step2

--- Part4/BlogList/utils/list_helper.js
+++ Part4/BlogList/utils/list_helper.js
@@ -2,7 +2,14 @@ const dummy=(blogs) => {
 	return 1
 }
 
+const totalLikes=(blogs) => {
+	return blogs.reduce((sum,item) => {
+		sum+=item.likes
+		return sum
+	},0)
+}
 
 module.exports={
-	dummy
+	dummy,
+	totalLikes
 }
