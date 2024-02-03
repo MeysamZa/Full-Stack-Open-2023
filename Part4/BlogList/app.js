7c4489db3b4d2ba87dc2df0be59463af3dd93f29
Meysam Zahedi <Maysam.t.zahedi@gmail.com>
2024-02-03 20:17:29 +0330

    4.18: bloglist expansion, step6

--- Part4/BlogList/app.js
+++ Part4/BlogList/app.js
@@ -4,8 +4,9 @@ const express = require('express')
 require('express-async-errors')
 const cors = require('cors')
 const blogsRouter=require('./controllers/blogs')
-const middleware=require('./utils/middleware')
+const middleware=require('./utils/middleware' )
 const usersRouter=require('./controllers/users')
+const loginRouter=require('./controllers/login')
 const mongoose = require('mongoose')
 
 
@@ -22,6 +23,7 @@ app.use(express.json())
 app.use(middleware.requestLogger)
 app.use('/api/users',usersRouter)
 app.use('/api/blogs',blogsRouter)
+app.use('/api/login',loginRouter)
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
 
