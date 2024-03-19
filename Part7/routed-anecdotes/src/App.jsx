c0398ea3f574f6a5c1f17567b6b217907752db23
Meysam Zahedi <Maysam.t.zahedi@gmail.com>
2024-03-19 20:54:51 +0330

    7.2: routed anecdotes, step2

--- Part7/routed-anecdotes/src/App.jsx
+++ Part7/routed-anecdotes/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import {Link,Routes,Route} from 'react-router-dom'
+import {Link,Routes,Route,useMatch} from 'react-router-dom'
 
 const Menu = () => {
   const padding = {
@@ -18,10 +18,23 @@ const AnecdoteList = ({ anecdotes }) => (
   <div>
     <h2>Anecdotes</h2>
     <ul>
-      {anecdotes.map(anecdote => <li key={anecdote.id} >{anecdote.content}</li>)}
+      {anecdotes.map(anecdote => 
+        <li key={anecdote.id} >
+          <Link to={`/anecdotes/${anecdote.id}`}>{anecdote.content}</Link>
+        </li>)}
     </ul>
   </div>
 )
+
+const Anecdote=({anecdote}) => {
+  return(
+    <div>
+      <h2>{anecdote.content}</h2>
+      <p>has {anecdote.votes} votes</p>
+      <p>for more info see <a href={anecdote.info}>{anecdote.info}</a></p>
+    </div>
+  )
+}
 
 const About = () => (
   <div>
@@ -31,7 +44,7 @@ const About = () => (
     <em>An anecdote is a brief, revealing account of an individual person or an incident.
       Occasionally humorous, anecdotes differ from jokes because their primary purpose is not simply to provoke laughter but to reveal a truth more general than the brief tale itself,
       such as to characterize a person by delineating a specific quirk or trait, to communicate an abstract idea about a person, place, or thing through the concrete details of a short narrative.
-      An anecdote is "a story with a point."</em>
+      An anecdote is &quot;a story with a point.&quot;</em>
 
     <p>Software engineering is full of excellent anecdotes, at this app you can find the best and add more.</p>
   </div>
@@ -125,6 +138,10 @@ const App = () => {
     setAnecdotes(anecdotes.map(a => a.id === id ? voted : a))
   }
 
+  const match=useMatch('/anecdotes/:id')
+  const anecdote=match
+                  ? anecdotes.find(item => item.id === Number(match.params.id))
+                  : null
   return (
     <div>
       <h1>Software anecdotes</h1>
@@ -133,6 +150,7 @@ const App = () => {
         <Route path='/' element={<AnecdoteList anecdotes={anecdotes} />}/>
         <Route path='/create' element={<CreateNew addNew={addNew} />}/>
         <Route path='/about' element={<About />}/>
+        <Route path='/anecdotes/:id' element={<Anecdote anecdote={anecdote}/>}/>
       </Routes>      
       <Footer />
     </div>
